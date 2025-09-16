2e818e42a22895d1240ec8b3baa6a55fd724bc73
hammad3074 <mh0332392344@gmail.com>
2025-09-17 04:54:04 +0500

    Update script.js

--- Grocery bill/script.js
+++ Grocery bill/script.js
@@ -1,46 +1,20 @@
-// Prices per kg
-let ricePrice = 250;    // per kg
-let beansPrice = 300;   // per kg
-let chickenPrice = 600; // per kg
+let ricePrice = 250;  
+let beansPrice = 300; 
+let chickenPrice = 600;
 
-// Total bill start
-let totalBill = 0;1``1
-let billDetails = "";
+alert("Welcome To Bin Hashim.!");
+let riceKg = parseFloat(prompt("How many kilograms of rice do you want?"));
+let beansKg = parseFloat(prompt("How many kilograms of beans do you want?"));
+let chickenKg = parseFloat(prompt("How many kilograms of chicken do you want?"));
 
-// 3 items ke liye loop
-for (let i = 1; i <= 3; i++) {
-    let choice = prompt(
-        "Choose a product:\n" +
-        "1 - Rice (Rs " + ricePrice + " per kg)\n" +
-        "2 - Beans (Rs " + beansPrice + " per kg)\n" +
-        "3 - Chicken (Rs " + chickenPrice + " per kg)"
-    );
+let riceBill = riceKg * ricePrice;
+let beansBill = beansKg * beansPrice;
+let chickenBill = chickenKg * chickenPrice;
 
-    if (choice === "1") {
-        let riceKg = parseFloat(prompt("You selected Rice.\nPrice: Rs " + ricePrice + " per kg\nHow many kilograms do you want?"));
-        let riceBill = riceKg * ricePrice;
-        totalBill += riceBill;
-        billDetails += "Rice: " + riceKg + " kg = Rs " + riceBill + "<br>";
-    } 
-    else if (choice === "2") {
-        let beansKg = parseFloat(prompt("You selected Beans.\nPrice: Rs " + beansPrice + " per kg\nHow many kilograms do you want?"));
-        let beansBill = beansKg * beansPrice;
-        totalBill += beansBill;
-        billDetails += "Beans: " + beansKg + " kg = Rs " + beansBill + "<br>";
-    } 
-    else if (choice === "3") {
-        let chickenKg = parseFloat(prompt("You selected Chicken.\nPrice: Rs " + chickenPrice + " per kg\nHow many kilograms do you want?"));
-        let chickenBill = chickenKg * chickenPrice;
-        totalBill += chickenBill;
-        billDetails += "Chicken: " + chickenKg + " kg = Rs " + chickenBill + "<br>";
-    } 
-    else {
-        alert("Invalid choice! Please type 1, 2, or 3.");
-        i--; // repeat this turn
-    }
-}
+let totalBill = riceBill + beansBill + chickenBill;
 
-// Show final bill
 document.write("<h3>Bill Details:</h3>");
-document.write(billDetails);
-document.write("<br><b>Total Bill = Rs " + totalBill + "</b>");
+document.write("Rice: " + riceKg + " kg = Rs " + riceBill + "<br>");
+document.write("Beans: " + beansKg + " kg = Rs " + beansBill + "<br>");
+document.write("Chicken: " + chickenKg + " kg = Rs " + chickenBill + "<br><br>");
+document.write("<b>Total Bill = Rs " + totalBill + "</b>");
