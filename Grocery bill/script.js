868868c49e166a112cdaa17d55352bc0a23961f9
hammad3074 <mh0332392344@gmail.com>
2025-09-17 09:44:04 +0500

    Update script.js

--- Grocery bill/script.js
+++ Grocery bill/script.js
@@ -3,9 +3,10 @@ let beansPrice = 300;
 let chickenPrice = 800;
 
 alert("Welcome To Bin Hashim.!");
-let riceKg = parseFloat(prompt("How many kilograms of rice do you want?"));
-let beansKg = parseFloat(prompt("How many kilograms of beans do you want?"));
-let chickenKg = parseFloat(prompt("How many kilograms of chicken do you want?"));
+
+let riceKg = parseFloat(prompt("Rice is Rs " + ricePrice + " per kg.\nHow many kilograms of rice do you want?"));
+let beansKg = parseFloat(prompt("Beans are Rs " + beansPrice + " per kg.\nHow many kilograms of beans do you want?"));
+let chickenKg = parseFloat(prompt("Chicken is Rs " + chickenPrice + " per kg.\nHow many kilograms of chicken do you want?"));
 
 let riceBill = riceKg * ricePrice;
 let beansBill = beansKg * beansPrice;
@@ -14,7 +15,10 @@ let chickenBill = chickenKg * chickenPrice;
 let totalBill = riceBill + beansBill + chickenBill;
 
 document.write("<h3>Bill Details:</h3>");
-document.write("Rice: " + riceKg + " kg = Rs " + riceBill + "<br>");
-document.write("Beans: " + beansKg + " kg = Rs " + beansBill + "<br>");
-document.write("Chicken: " + chickenKg + " kg = Rs " + chickenBill + "<br><br>");
+document.write("-----------Bin Hashim------------" + "<br>");
+document.write("Rice Quantity: " + riceKg + " kg x " + ricePrice + " = Rs " + riceBill + "<br>");
+document.write("Beans Quantity: " + beansKg + " kg x " + beansPrice + " = Rs " + beansBill + "<br>");
+document.write("Chicken quantity: " + chickenKg + " kg x " + chickenPrice + " = Rs " + chickenBill + "<br><br>");
 document.write("<b>Total Bill = Rs " + totalBill + "</b>");
+
+
