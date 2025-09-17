abef49af5ae95471854b9af29057d3d78da0378c
hammad3074 <mh0332392344@gmail.com>
2025-09-17 05:02:22 +0500

    Js Assignment

--- Roll No Search/script.js
+++ Roll No Search/script.js
@@ -1,37 +1,8 @@
-// // Function to search roll number
-// function searchRollNumber(rollNumbers, searchValue) {
-//     let found = false;
-
-//     for (let i = 0; i < rollNumbers.length; i++) {
-//         if (rollNumbers[i] === searchValue) {   // if statement
-//             found = true;
-//             break;
-//         }
-//     }
-
-//     if (found) {
-//         document.write("✅ Roll number " + searchValue + " is found in the list.");
-//     } else {
-//         document.write("❌ Roll number " + searchValue + " is NOT found in the list.");
-//     }
-// }
-
-// // Roll numbers list
-// let rollNumbers = [101, 102, 103, 104, 105];
-
-// // User input
-// let searchValue = parseInt(prompt("Enter roll number to search:"));
-
-// // Call function
-// searchRollNumber(rollNumbers, searchValue);
-
-
-// Function to search roll number
 function searchRollNumber(rollNumbers, searchValue) {
     let found = false;
 
     for (let i = 0; i < rollNumbers.length; i++) {
-        if (rollNumbers[i] === searchValue) {   // if statement
+        if (rollNumbers[i] === searchValue) {
             found = true;
             break;
         }
@@ -44,11 +15,6 @@ function searchRollNumber(rollNumbers, searchValue) {
     }
 }
 
-// Roll numbers list
 let rollNumbers = [101, 102, 103, 104, 105];
-
-// User input
 let searchValue = parseInt(prompt("Enter roll number to search:"));
-
-// Call function
 searchRollNumber(rollNumbers, searchValue);
